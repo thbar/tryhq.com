ccb37f80fbebc3f3e21aad47bdabf42664996afe
Thibaut Barrère <thibaut.barrere@gmail.com>
2011-05-09 22:02:50 +0200

    Gathering the online interpreters I've been able to find so far.

---
+++ data.js
@@ -0,0 +1,92 @@
+var TryHQ = [
+  {
+    name: "MongoDB",
+    tags: ["database", "nosql"],
+    description: "...",
+    try_url: "http://try.mongodb.org"
+  },
+  {
+    name: "Erlang",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://www.tryerlang.org",
+  },
+  {
+    name: "Haskell",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://tryhaskell.org"
+  },
+  {
+    name: "Clojure",
+    tags: ["language"],
+    description: "A functional programming language that targets the Java Virtual Machine. It shares Lisp's code-as-data philosophy and powerful macro system",
+    try_url: "http://www.try-clojure.org"
+  },
+  {
+    name: "Redis",
+    tags: ["database", "nosql"],
+    description: "...",
+    try_url: "http://try.redis-db.com"
+  },
+  {
+    name: "Ruby",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://tryruby.org"
+  },
+  {
+    name: "Scala",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://www.simplyscala.com"
+  },
+  {
+    name: "Javascript",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://jsbin.com"
+  },
+  {
+    name: "CSS",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://cssdesk.com"
+  },
+  {
+    name: "Haml",
+    tags: ["language"],
+    description: "Haml is a markup language that is used to describe the XHTML of any web document without the use of traditional inline coding",
+    try_url: "http://haml-lang.com/try.html"
+  },
+  { 
+    name: "Sass",
+    tags: ["language"],
+    description: "Sass is a meta-language on top of CSS. It supports variables, nesting, mixins and selector inheritance.",
+    try_url: "http://sass-lang.com/try.html"
+  },
+  {
+    name: "CoffeeScript",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://jashkenas.github.com/coffee-script"
+  },
+  {
+    name: "LUA",
+    tags: ["language"],
+    description: "...",
+    try_url: "http://www.lua.org/demo.html"
+  },
+  {
+    name: "Fargo",
+    tags: ["language"],
+    description: "An experimental language to improve how we work with asynchronous systems in JavaScript. It runs on Node.js and in browsers.",
+    try_url: "http://fargo.jcoglan.com"
+  },
+  {
+    name: "Python",
+    tags: ["language", "dynamic"],
+    description: "This tutorial requires Silverlight.",
+    try_url: "http://www.trypython.org"
+  }
+];
